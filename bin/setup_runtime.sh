6987e986735e6b2fd5cb2eb38d4b2371c013c0f4
Thomas Maxwell <thomas.maxwell@nasa.gov>
2016-06-13 14:03:29 -0400

    Add setup

--- bin/setup_runtime.sh
+++ bin/setup_runtime.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export CDAS_BIN_DIR="$( dirname "${BASH_SOURCE[0]}" )"
+export CDAS_BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export CDAS_MAIN_DIR=${CDAS_BIN_DIR}/../src/main
 export CDAS_SCALA_DIR=${CDAS_MAIN_DIR}/scala
 export CDAS_RESOURCES_DIR=${CDAS_MAIN_DIR}/resources
