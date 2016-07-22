16798042c51659be23cb1e704aed1375aec5f2f7
Thomas Maxwell <thomas.maxwell@nasa.gov>
2016-07-22 17:24:02 -0400

    Add nohup

--- bin/setup_runtime.sh
+++ bin/setup_runtime.sh
@@ -9,9 +9,11 @@ export CDSHELL_HOME_DIR=${CDSHELL_HOME_DIR:-${CDAS_HOME_DIR}/../CDASClientConsol
 export CDAS_SCALA_DIR=${CDAS_BIN_DIR}/../src/main/scala
 export PATH=${CDAS_BIN_DIR}:${PATH}
 export CLASSPATH=${CDAS_SCALA_DIR}:${CDAS_CACHE_DIR}:${CLASSPATH}
+WPS_CMD='$CDWPS_HOME_DIR/target/universal/cdwps-1.1-SNAPSHOT/bin/cdwps -J-Xmx6000M -J-Xms512M -J-Xss1M -J-XX:+CMSClassUnloadingEnabled -J-XX:+UseConcMarkSweepGC -J-XX:MaxPermSize=800M'
 
 alias cdas='cd $CDAS_HOME_DIR'
 alias cdist='cd $CDWPS_HOME_DIR; sbt dist; cd target/universal/; rm -rf cdwps-*-SNAPSHOT; unzip *.zip'
-alias cdwps='nohup $CDWPS_HOME_DIR/target/universal/cdwps-1.1-SNAPSHOT/bin/cdwps -J-Xmx6000M -J-Xms512M -J-Xss1M -J-XX:+CMSClassUnloadingEnabled -J-XX:+UseConcMarkSweepGC -J-XX:MaxPermSize=800M &'
+alias cdwps='$WPS_CMD'
+alias cdwpsb='nohup $WPS_CMD &'
 alias pcdas='cd $CDAS_HOME_DIR; git fetch; git pull; sbt publish-local'
 alias cdshell='cd $CDSHELL_HOME_DIR; sbt run'
