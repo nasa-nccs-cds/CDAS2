440543e5e54b2bf24cd70ab24cec72afccd04910
Thomas Maxwell <thomas.maxwell@nasa.gov>
2016-08-20 15:06:18 -0400

    Add test suite.

--- src/test/scala/cdas/wps/executionTest.scala
+++ src/test/scala/cdas/wps/executionTest.scala
@@ -7,6 +7,9 @@ import scala.io.Source
 import org.scalatest.Tag
 
 // Execution Example (tag = yearly_cycle):   >> sbt test-only *wpsSuite -- -n yearly_cycle
+// Define frag id in ~/.cdas/test_config.txt
+// sample test_config.txt:
+// fragment=t|merra/daily|0,0,0,0|248,42,144,288
 
 class wpsSuite extends LocalExecutionTestSuite {
   val fragment = getConfigValue("fragment")
