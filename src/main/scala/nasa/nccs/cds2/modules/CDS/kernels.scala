64f519dc4b001008774c3a5e686b8f0c9cb99c72
Thomas Maxwell <thomas.maxwell@nasa.gov>
2016-08-23 11:25:37 -0400

    Add diagnostics

--- src/main/scala/nasa/nccs/cds2/modules/CDS/kernels.scala
+++ src/main/scala/nasa/nccs/cds2/modules/CDS/kernels.scala
@@ -184,7 +184,7 @@ class CDS extends KernelModule with KernelTools {
           case wtype => context.server.getAxisData(inputVar.fragmentSpec, 'y').map(axis_data => dataFrag.data.computeWeights(wtype, Map('y' -> axis_data)))
         }
         val anomaly_result: CDFloatArray = dataFrag.data.anomaly(axes.args, weightsOpt)
-        logger.info( "Partition[%d], generated anomaly result: shape = [ %s ]".format(partIndex,anomaly_result.getShape.mkString(",") ) )
+        logger.info( "Partition[%d], generated anomaly result: %s".format(partIndex, anomaly_result.toDataString ) )
         val t11 = System.nanoTime
         new DataFragment(resultFragSpec, anomaly_result)
       }
