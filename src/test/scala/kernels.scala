a0dba37e5c982fea0456518d20295531d4765a25
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2016-11-04 13:03:39 -0400

    Edit test.

--- src/test/scala/kernels.scala
+++ src/test/scala/kernels.scala
@@ -26,8 +26,8 @@ class CurrentTestSuite extends TestSuite(0, 0, 0f, 0f ) with Loggable {
 //  }
 
   test("EnsembleAve") {
-    val variables = ( 1 to 6 ) map { index => s"""{"uri":"collection:/GISS_r${index}i1p1","name":"tas:v$index","domain":"d0"}""" }
-    val vids = ( 1 to 6 ) map { index => s"v$index" }
+    val variables = ( 1 to 5 ) map { index => s"""{"uri":"collection:/GISS_r${index}i1p1","name":"tas:v$index","domain":"d0"}""" }
+    val vids = ( 1 to 5 ) map { index => s"v$index" }
     val datainputs = """[domain=[{"name":"d0"}],variable=[%s],operation=[{"name":"CDSpark.multiAverage","input":"%s","domain":"d0"}]]""".format( variables.mkString(","), vids.mkString(",") )
     logger.info( "Request datainputs: " + datainputs )
     val result_node = executeTest(datainputs)
