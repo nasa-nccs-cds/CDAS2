6b413a66376543b49207a94a2e289b80c3996a47
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-01-25 13:20:59 -0500

    regrid

--- src/main/scala/nasa/nccs/cdapi/cdm/TimeRegridder.scala
+++ src/main/scala/nasa/nccs/cdapi/cdm/TimeRegridder.scala
@@ -2,11 +2,17 @@ package nasa.nccs.cdapi.cdm
 import ucar.nc2.dataset.CoordinateAxis1DTime
 import ucar.ma2
 
-class TimeRegridder( val fromAxis: CoordinateAxis1DTime, val toAxis: CoordinateAxis1DTime, val range: ma2.Range = ma2.Range.EMPTY ) {
+class TimeRegridder( val toAxis: CoordinateAxis1DTime, val fromAxis: CoordinateAxis1DTime, val toAxisRange: ma2.Range = ma2.Range.EMPTY ) {
   val weights = computeWeights()
 
   def computeWeights() = {
-    val actualRange = if( range == ma2.Range.EMPTY ) { Range(0,fromAxis.getSize.toInt-1) } else range
+    val actualRange = if( toAxisRange == ma2.Range.EMPTY ) { new ma2.Range(toAxis.getSize.toInt) } else toAxisRange
+    val iter  = actualRange.getIterator
+    while ( iter.hasNext ) {
+      val index = iter.next
+      val cdate0 = toAxis.getCalendarDate(index)
+      val fromIndex = fromAxis.findTimeIndexFromCalendarDate(cdate0)
+    }
 
   }
 
