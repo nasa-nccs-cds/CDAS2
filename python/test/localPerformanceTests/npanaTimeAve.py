b40181a8fd01389bd387981dafe8adc969f5255f
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-03-04 12:23:50 -0500

    Update performance test

--- python/test/localPerformanceTests/npanaTimeAve.py
+++ python/test/localPerformanceTests/npanaTimeAve.py
@@ -4,6 +4,8 @@ import time
 try:
     portal = CDASPortal()
     response_manager = portal.createResponseManager()
+    portal.start_CDAS()
+    time.sleep(6)
 
     t0 = time.time()
     datainputs = """[domain=[{"name":"d0","lat":{"start":205,"end":210,"system":"indices"}},{"name":"d1","lon":{"start":205,"end":215,"system":"indices"},"lev":{"start":5,"end":5,"system":"indices"}}],variable=[{"uri":"collection:/npana","name":"T:v1","domain":"d0"}],operation=[{"name":"CDSpark.average","input":"v1","domain":"d1","axes":"t"}]]"""
