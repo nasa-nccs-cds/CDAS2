13529a6f33d678293942f5bfa2ae29d7a8c9236b
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-03-04 12:34:20 -0500

    Update performance test

--- python/test/localPerformanceTests/npanaTimeAve.py
+++ python/test/localPerformanceTests/npanaTimeAve.py
@@ -8,7 +8,7 @@ try:
     time.sleep(6)
 
     t0 = time.time()
-    datainputs = """[domain=[{"name":"d0","lat":{"start":205,"end":210,"system":"indices"}},{"name":"d1","lon":{"start":205,"end":215,"system":"indices"},"lev":{"start":5,"end":5,"system":"indices"}}],variable=[{"uri":"collection:/npana","name":"T:v1","domain":"d0"}],operation=[{"name":"CDSpark.average","input":"v1","domain":"d1","axes":"t"}]]"""
+    datainputs = """[domain=[{"name":"d0","lat":{"start":205,"end":210,"system":"indices"},"lon":{"start":205,"end":215,"system":"indices"}},{"name":"d1","lev":{"start":5,"end":5,"system":"indices"}}],variable=[{"uri":"collection:/npana","name":"T:v1","domain":"d1"}],operation=[{"name":"CDSpark.average","input":"v1","domain":"d0","axes":"t"}]]"""
     rId = portal.sendMessage("execute", [ "CDSpark.workflow", datainputs, ""] )
     responses = response_manager.getResponses(rId)
     print "Completed OP in time {0}, Got responses:\n{1}".format( time.time()-t0, "\n".join(responses) )
