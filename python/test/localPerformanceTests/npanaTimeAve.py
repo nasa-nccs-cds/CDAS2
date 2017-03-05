7ff6df0bb3e7d40e6ac11ce8500fda0122e55c1e
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-03-05 12:57:09 -0500

    Update performance test

--- python/test/localPerformanceTests/npanaTimeAve.py
+++ python/test/localPerformanceTests/npanaTimeAve.py
@@ -1,5 +1,5 @@
 from pycdas.portal.cdas import CDASPortal
-import time
+import time, sys
 
 startServer = False
 
@@ -12,10 +12,10 @@ try:
 
     t0 = time.time()
     datainputs = """[domain=[{"name":"d0","lat":{"start":205,"end":210,"system":"indices"},"lon":{"start":205,"end":215,"system":"indices"}},{"name":"d1","lev":{"start":5,"end":5,"system":"indices"}}],variable=[{"uri":"collection:/npana","name":"T:v1","domain":"d1"}],operation=[{"name":"CDSpark.average","input":"v1","domain":"d0","axes":"t"}]]"""
-    print "Sending request (awaiting response): '" + datainputs + "'"
+    print "Sending request (awaiting response): '" + datainputs + "'"; sys.stdout.flush()
     rId = portal.sendMessage( "execute", [ "CDSpark.workflow", datainputs, ""] )
     responses = response_manager.getResponses(rId)
-    print "Completed OP in time {0}, Got responses:\n{1}".format( time.time()-t0, "\n".join(responses) )
+    print "Completed OP in time {0}, Got responses:\n{1}".format( time.time()-t0, "\n".join(responses) ); sys.stdout.flush()
 
 finally:
 
