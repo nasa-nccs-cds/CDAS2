7adbbca46baf34698348105f085afc5fa16e232a
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-03-07 17:27:02 -0500

    Fix section indexing issue

--- src/main/scala/nasa/nccs/caching/fileCache.scala
+++ src/main/scala/nasa/nccs/caching/fileCache.scala
@@ -85,7 +85,7 @@ class Partition(val index: Int, val path: String, val dimIndex: Int, val startIn
     runtime.printMemoryUsage(logger)
     new CDFloatArray(shape, buffer.asFloatBuffer, missing_value)
   }
-  val partitionOrigin: Array[Int] = origin.zipWithIndex map { case (value, ival) => if( ival == 0 ) value + startIndex else ival }
+  val partitionOrigin: Array[Int] = origin.zipWithIndex map { case (value, ival) => if( ival == 0 ) value + startIndex else value }
   def delete = { FileUtils.deleteQuietly(new File(path)) }
   def chunkSection(iChunk: Int, section: ma2.Section): ma2.Section = {
     new ma2.Section(section.getRanges).replaceRange(dimIndex, chunkRange(iChunk)).intersect(section)
