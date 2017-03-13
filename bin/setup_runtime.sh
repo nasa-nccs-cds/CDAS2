f9fc56865c69cd74300d0d0a937ab97dd3ace638
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-03-13 17:12:28 -0400

    stage

--- bin/setup_runtime.sh
+++ bin/setup_runtime.sh
@@ -10,8 +10,9 @@ export CDAS_SCALA_DIR=${CDAS_BIN_DIR}/../src/main/scala
 export CDAS_STAGE_DIR=${CDAS_HOME_DIR}/target/universal/stage
 export CLASSPATH=${CDAS_SCALA_DIR}:${CDAS_CACHE_DIR}:${CDAS_STAGE_DIR}/conf:${CDAS_STAGE_DIR}/lib:${CONDA_PREFIX}/lib:${CLASSPATH}
 export UVCDAT_ANONYMOUS_LOG=no
-export WPS_CMD="$CDWPS_HOME_DIR/target/universal/cdwps-1.1-SNAPSHOT/bin/cdwps -J-Xmx$CDAS_MAX_MEM -J-Xms512M -J-Xss1M -J-XX:+CMSClassUnloadingEnabled -J-XX:+UseConcMarkSweepGC"
-# export PYTHONPATH=${PYTHONPATH}:${CDAS_HOME_DIR}/python
+export CDAS_JAVA_ARGS="-J-Xmx$CDAS_MAX_MEM -J-Xms512M -J-Xss1M -J-XX:+CMSClassUnloadingEnabled -J-XX:+UseConcMarkSweepGC"
+export WPS_CMD="$CDWPS_HOME_DIR/target/universal/cdwps-1.1-SNAPSHOT/bin/cdwps $CDAS_JAVA_ARGS"
+export CDSHELL_CMD="$CDSHELL_HOME_DIR/target/universal/stage/bin/cdasclientconsole $CDAS_JAVA_ARGS"
 export PATH=${CDAS_STAGE_DIR}/bin:${CDAS_BIN_DIR}:${PATH}
 
 alias cdas='cd $CDAS_HOME_DIR'
@@ -20,9 +21,9 @@ alias pdist='cd $CDAS_HOME_DIR; sbt universal:packageBin; cd target/universal/; 
 alias cdwps=$WPS_CMD
 alias cdwpsb='nohup $WPS_CMD &'
 alias pcdas='cd $CDAS_HOME_DIR; git fetch; git pull; sbt publish'
-alias cdshd='unset CDAS_SERVER_ADDRESS; unset CDAS_SERVER_PORT; cd $CDSHELL_HOME_DIR; sbt run'
-alias cdshw='export CDAS_SERVER_ADDRESS=localhost; unset CDAS_SERVER_PORT; cd $CDSHELL_HOME_DIR; sbt run'
-alias cdshr='export CDAS_SERVER_ADDRESS=localhost; export CDAS_SERVER_PORT=9001; cd $CDSHELL_HOME_DIR; sbt run'
+alias cdshd='unset CDAS_SERVER_ADDRESS; unset CDAS_SERVER_PORT; $CDSHELL_CMD'
+alias cdshw='export CDAS_SERVER_ADDRESS=localhost; unset CDAS_SERVER_PORT; $CDSHELL_CMD'
+alias cdshr='export CDAS_SERVER_ADDRESS=localhost; export CDAS_SERVER_PORT=9001; $CDSHELL_CMD'
 alias cdup='cd $CDAS_HOME_DIR; ./bin/update.sh; rm ~/.cdas/*.log; python ./python/src/pycdas/shutdown.py'
 alias cdupy='cd $CDAS_HOME_DIR; python setup.py install; rm ~/.cdas/*.log; python ./python/src/pycdas/shutdown.py'
 alias rncml='rm $CDAS_CACHE_DIR/collections/NCML/*'
