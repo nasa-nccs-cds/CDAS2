bc5c470030021a3d911d6ab1b32d1de5e33cec53
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-03-15 12:24:06 -0400

    Expand performance test

--- src/main/scala/nasa/nccs/cdapi/cdm/dataset.scala
+++ src/main/scala/nasa/nccs/cdapi/cdm/dataset.scala
@@ -616,7 +616,7 @@ object profilingTest extends Loggable {
     if (max == Float.MinValue) Float.NaN else max
   }
 
-  def computeMax(data: ma2.Array): Float = { val array = CDFloatArray.factory(data,Float.NaN); array.getSize.toFloat }
+  def computeMax(data: ma2.Array): Float = { val array = CDFloatArray.factory(data,Float.NaN).max(); array.getSize.toFloat }
 
   def computeMax( data: CDFloatArray ): Float = {
     var max = Float.MinValue
