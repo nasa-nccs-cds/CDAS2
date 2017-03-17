af69b648bcd2c8ded39eac26816f8ffd246c4b85
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-03-17 16:18:07 -0400

    Update performance test

--- src/main/scala/nasa/nccs/cdapi/cdm/dataset.scala
+++ src/main/scala/nasa/nccs/cdapi/cdm/dataset.scala
@@ -12,6 +12,7 @@ import java.util.Formatter
 import scala.concurrent.ExecutionContext.Implicits.global
 import com.googlecode.concurrentlinkedhashmap.ConcurrentLinkedHashMap
 import nasa.nccs.caching.CDASPartitioner
+import nasa.nccs.cdapi.data.HeapFltArray
 import nasa.nccs.cdapi.tensors.{CDDoubleArray, CDFloatArray, CDLongArray}
 import nasa.nccs.cdas.loaders.XmlResource
 import nasa.nccs.cdas.utilities.appParameters
@@ -28,6 +29,8 @@ import scala.reflect.ClassTag
 import scala.xml.XML
 import org.apache.commons.io.IOUtils
 import nasa.nccs.cdas.loaders.Collections
+import nasa.nccs.cdas.workers.TransVar
+import nasa.nccs.cdas.workers.python.{PythonWorker, PythonWorkerPortal}
 import nasa.nccs.esgf.process.DataSource
 import ucar.nc2._
 import ucar.nc2.ncml.NcMLReader
@@ -666,12 +669,12 @@ object profilingTest extends Loggable {
     println("Completed data processing for collection '%s' in %.4f sec".format(partitioner.cache_id, (System.nanoTime() - t0) / 1.0E9))
   }
 
-  def processFileData(ncmlFile: String, varName: String) = {
+  def processFileData(ncmlFile: String, gridFile: String, varName: String) = {
     try {
       val datset = NetcdfDataset.openDataset(ncmlFile, true, -1, null, null)
       Option(datset.findVariable(varName)) match {
         case None => throw new IllegalStateException("Variable '%s' was not loaded".format(varName))
-        case Some(ncVar) => processData(ncVar)
+        case Some(ncVar) => processDataPython(ncVar,gridFile)
       }
     } catch {
       case e: java.io.IOException =>
@@ -681,6 +684,40 @@ object profilingTest extends Loggable {
         logger.error("Something went wrong while reading %s".format(ncmlFile))
         throw ex
     }
+  }
+
+  def processDataPython(variable: Variable, gridFile: String) = {
+    val workerManager: PythonWorkerPortal  = PythonWorkerPortal.getInstance();
+    val worker: PythonWorker = workerManager.getPythonWorker();
+    val t0 = System.nanoTime()
+    val full_shape = variable.getShape
+    var total_read_time = 0.0
+    var total_compute_time = 0.0
+    val chunk_size = 1
+    println("Processing data, full shape = " + full_shape.mkString(", "))
+    (0 until full_shape(1)) foreach (ilevel => {
+      (0 until full_shape(0) by chunk_size) foreach (itime => {
+        val ncycle = ilevel * full_shape(0) + itime + 1
+        val chunk_origin = Array[Int](itime, ilevel, 0, 0)
+        val chunk_shape = Array[Int](chunk_size, 1, full_shape(2), full_shape(3))
+        val ts0 = System.nanoTime()
+        val data = variable.read(chunk_origin, chunk_shape)
+        val ts1 = System.nanoTime()
+        worker.sendRequestInput( variable.getShortName, HeapFltArray( data, chunk_origin, gridFile, Map.empty[String,String], Float.NaN ) )
+        worker.sendRequest("numpyModule.max", Array(variable.getShortName), Map.empty[String,String] )
+        val tvar: TransVar = worker.getResult()
+        val result = HeapFltArray( tvar, Some(Float.NaN), Some(gridFile) )
+        val ts2 = System.nanoTime()
+        val read_time = (ts1 - ts0) / 1.0E9
+        val compute_time = (ts2 - ts1) / 1.0E9
+        total_read_time += read_time
+        total_compute_time += compute_time
+        println("Computed max = %.4f [time=%d, level=%d] in %.4f sec, data read time = %.4f sec, compute time = %.4f sec".format( result.data(0), itime, ilevel, read_time + compute_time, read_time, compute_time))
+        println("Aggretate time for %d cycles = %.4f sec".format(ncycle, (ts2 - t0) / 1.0E9))
+        println("Average over %d cycles: read time per tstep = %.4f sec, compute time per tstep = %.4f sec".format(ncycle, total_read_time / (ncycle*chunk_size), total_compute_time / (ncycle*chunk_size) ))
+      })
+    })
+    println("Completed data processing for '%s' in %.4f sec".format(variable.getFullName, (System.nanoTime() - t0) / 1.0E9))
   }
 
 
@@ -715,13 +752,14 @@ object profilingTest extends Loggable {
 
   def main(args: Array[String]): Unit = {
     val ncmlFile = "/att/gpfsfs/ffs2004/ppl/tpmaxwel/cdas/cache/collections/NCML/npana.xml"
+    val gridFile = "/att/gpfsfs/ffs2004/ppl/tpmaxwel/cdas/cache/collections/NCML/npana.nc"
     val cache_id = "a3298cb50c2abb"
     val varName = "T"
     val iLevel = 10
     val roi_origin = Array[Int](0, iLevel, 0, 0)
     val roi_shape = Array[Int](53668, 1, 361, 576)
     val roi = new ma2.Section(roi_origin, roi_shape)
-    processFileData( ncmlFile, varName )
+    processFileData( ncmlFile, gridFile, varName )
   }
 }
 
