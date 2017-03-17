43029956b19e9b6358c832bbb452eab35aad627f
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-03-17 18:05:54 -0400

    Update performance test

--- src/main/scala/nasa/nccs/cdapi/cdm/dataset.scala
+++ src/main/scala/nasa/nccs/cdapi/cdm/dataset.scala
@@ -694,8 +694,10 @@ object profilingTest extends Loggable {
     var total_read_time = 0.0
     var total_compute_time = 0.0
     val chunk_size = 10
+    val attrs = variable.getAttributes.iterator().map( _.getShortName ).mkString(", ")
     val mem_size = (chunk_size*4*full_shape(2)*full_shape(3))/1.0E6
-    println("Processing data, full shape = " + full_shape.mkString(", "))
+    val missing = variable.findAttributeIgnoreCase("fmissing_value").getNumericValue.floatValue()
+    println("Processing data, full shape = %s, attrs = %s".format( full_shape.mkString(", "), attrs ))
     (0 until full_shape(1)) foreach (ilevel => {
       (0 until full_shape(0) by chunk_size) foreach (itime => {
         val ncycle = ilevel * full_shape(0) + itime + 1
@@ -705,21 +707,21 @@ object profilingTest extends Loggable {
         val data = variable.read(chunk_origin, chunk_shape)
         val ts1 = System.nanoTime()
         val rID = "r" + ncycle.toString
-        val attrs = variable.getAttributes.iterator().map( _.getShortName ).mkString(", ")
+
         val metadata: Map[String, String] = Map( "name" -> variable.getShortName, "collection" -> "npana", "gridfile" -> gridFile, "dimensions" -> variable.getDimensionsString,
             "units" -> variable.getUnitsString, "longname" -> variable.getFullName, "uid" -> variable.getShortName, "roi" -> CDSection.serialize(new ma2.Section(chunk_origin,chunk_shape)) )
         val op_metadata: Map[String, String] = Map.empty[String,String] // Map( "axis" -> "x" ) // Map.empty[String,String]
-        worker.sendRequestInput( variable.getShortName, HeapFltArray( data, chunk_origin, gridFile, metadata, Float.NaN ) )
+        worker.sendRequestInput( variable.getShortName, HeapFltArray( data, chunk_origin, gridFile, metadata, missing ) )
         worker.sendRequest("python.numpyModule.max-"+rID, Array(variable.getShortName), op_metadata )
         val tvar: TransVar = worker.getResult()
-        val result = HeapFltArray( tvar, Some(Float.NaN), Some(gridFile) )
+        val result = HeapFltArray( tvar, Some(missing), Some(gridFile) )
         val ts2 = System.nanoTime()
         val read_time = (ts1 - ts0) / 1.0E9
         val compute_time = (ts2 - ts1) / 1.0E9
         total_read_time += read_time
         total_compute_time += compute_time
         println("Computed max = %.4f [time=%d, level=%d, nts=%d] in %.4f sec per ts, data read time per ts = %.4f sec, compute time per ts = %.4f sec".format( result.data(0), itime, ilevel, chunk_size, (read_time + compute_time)/chunk_size, read_time/chunk_size, compute_time/chunk_size))
-        println("Aggretate time for %d cycles = %.4f sec, chunk mem size = %.2f MB, attrs = %s".format( ncycle, (ts2 - t0) / 1.0E9, mem_size, attrs ))
+        println("Aggretate time for %d cycles = %.4f sec, chunk mem size = %.2f MB".format( ncycle, (ts2 - t0) / 1.0E9, mem_size ))
         println("Average over %d cycles: read time per tstep = %.4f sec, compute time per tstep = %.4f sec".format(ncycle, total_read_time / ncycle, total_compute_time / ncycle ))
       })
     })
