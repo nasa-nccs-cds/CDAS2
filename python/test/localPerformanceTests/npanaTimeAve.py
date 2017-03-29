83e01f0e99a0a466181d8ad7da1aea8de4e9749e
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-03-29 14:22:39 -0400

    Update npana test

--- python/test/localPerformanceTests/npanaTimeAve.py
+++ python/test/localPerformanceTests/npanaTimeAve.py
@@ -20,7 +20,7 @@ try:
     t0 = time.time()
 #    datainputs = """[domain=[{"name":"d0"}],variable=[{"uri":"file:/Users/tpmaxwel/.cdas/cache/collections/NCML/giss_r1i1p1.xml","name":"tas:v1","domain":"d0"}],operation=[{"name":"CDSpark.average","input":"v1","domain":"d0","axes":"tz"}]]"""
 #    datainputs = """[domain=[{"name":"d0","lev":{"start":10,"end":10,"system":"indices"}}],variable=[{"uri":"collection:/npana","name":"tas:v1","domain":"d0"}],operation=[{"name":"numpyModule.max","input":"v1","axes":"tx"}]]"""
-    datainputs = """[domain=[{"name":"d0","time":{"start":0,"end":1000,"system":"indices"}}],variable=[{"uri":"file://att/gpfsfs/ffs2004/ppl/tpmaxwel/cdas/cache/collections/NCML/npana.xml","name":"T:v1","domain":"d0"}],operation=[{"name":"cdmsModule.AverageKernel","input":"v1","domain":"d0","axes":"tz"}]]"""
+    datainputs = """[domain=[{"name":"d0","time":{"start":0,"end":1000,"system":"indices"}}],variable=[{"uri":"file://att/gpfsfs/ffs2004/ppl/tpmaxwel/cdas/cache/collections/NCML/npana.xml","name":"T:v1","domain":"d0"}],operation=[{"name":"python.cdmsModule.AverageKernel","input":"v1","domain":"d0","axes":"tz"}]]"""
     print "Sending request on port {0} (awaiting response): {1}".format( portal.request_port, datainputs ); sys.stdout.flush()
     rId = portal.sendMessage( "execute", [ "CDSpark.workflow", datainputs, ""] )
     responses = response_manager.getResponses(rId)
