e846690bd0f912356ff519c6f20fb441ad0af585
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-03-29 18:14:05 -0400

    Update npana test

--- python/test/localPerformanceTests/npanaTimeAve.py
+++ python/test/localPerformanceTests/npanaTimeAve.py
@@ -17,7 +17,7 @@ try:
 
     response_manager = portal.createResponseManager()
 
-    t0 = time.time() //
+    t0 = time.time()
 #    datainputs = """[domain=[{"name":"d0"}],variable=[{"uri":"file:/Users/tpmaxwel/.cdas/cache/collections/NCML/giss_r1i1p1.xml","name":"tas:v1","domain":"d0"}],operation=[{"name":"CDSpark.average","input":"v1","domain":"d0","axes":"tz"}]]"""
 #    datainputs = """[domain=[{"name":"d0","lev":{"start":10,"end":10,"system":"indices"}}],variable=[{"uri":"collection:/npana","name":"tas:v1","domain":"d0"}],operation=[{"name":"numpyModule.max","input":"v1","axes":"tx"}]]"""
 #    datainputs = """[domain=[{"name":"d0","time":{"start":0,"end":1000,"system":"indices"}}],variable=[{"uri":"file://att/gpfsfs/ffs2004/ppl/tpmaxwel/cdas/cache/collections/NCML/npana.xml","name":"T:v1","domain":"d0"}],operation=[{"name":"python.cdmsModule.ave","input":"v1","domain":"d0","axes":"tz"}]]"""
