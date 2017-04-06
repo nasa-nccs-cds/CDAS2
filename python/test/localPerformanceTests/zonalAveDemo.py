40029d6cfd71128a22b85c9ebe1ff9d76e6d27b9
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-04-06 14:13:37 -0400

    Update zaDemo.py

--- python/test/localPerformanceTests/zonalAveDemo.py
+++ python/test/localPerformanceTests/zonalAveDemo.py
@@ -18,16 +18,14 @@ try:
     response_manager = portal.createResponseManager()
 
     t0 = time.time()
-    data_path1 = "file:///Users/tpmaxwel/Dropbox/Tom/Data/MERRA/atmos_ua.nc"
-    data_path = "file:/att/gpfsfs/ffs2004/ppl/tpmaxwel/cdas/cache/collections/NCML/merra_mon_ua.xml"
-    datainputs = '[domain=[{"name":"d0"}],variable=[{"uri":"{0}","name":"ua:v1","domain":"d0"}],operation=[{"name":"python.cdmsModule.ave","input":"v1","axes":"xt","filter":"DJF"}]]'.format(data_path)
+    datainputs = '[domain=[{"name":"d0"}],variable=[{"uri":"file:/att/gpfsfs/ffs2004/ppl/tpmaxwel/cdas/cache/collections/NCML/merra_mon_ua.xml","name":"ua:v1","domain":"d0"}],operation=[{"name":"python.cdmsModule.ave","input":"v1","axes":"xt","filter":"DJF"}]]'
     print "Sending request on port {0}: {1}".format( portal.request_port, datainputs ); sys.stdout.flush()
     rId = portal.sendMessage( "execute", [ "CDSpark.workflow", datainputs, ""] )
     responses = response_manager.getResponses(rId)
     print "Completed OP in time {0}".format( time.time()-t0 ); sys.stdout.flush()
 
 except Exception, err:
-    print "Exception occurred: " + str(err)
+    traceback.print_exc()
 
 finally:
 
