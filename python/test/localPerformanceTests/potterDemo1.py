3c2ade45fe7d3f8bb17bf11f4f3e459b6e2355ee
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-04-10 13:14:07 -0400

    Update potter demo

--- python/test/localPerformanceTests/potterDemo1.py
+++ python/test/localPerformanceTests/potterDemo1.py
@@ -1,5 +1,5 @@
 #get path
-pth1='/att/gpfsfs/ffs2004/ppl/tpmaxwel/cdas/cache/collections/NCML/merra_mon_ua.xml'
+pth1='/home/tpmaxwel/.cdas/cache/cdscan/merra_mon_ua.xml'
 #pth1='https://dataserver.nccs.nasa.gov/thredds/dodsC/bypass/CREATE-IP/reanalysis/MERRA2/mon/atmos/ua.ncml'
 #pth2='https://dataserver.nccs.nasa.gov/thredds/dodsC/bypass/CREATE-IP/reanalysis/MERRA/mon/atmos/ua.ncml'
 # variable
