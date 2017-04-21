9fc80c295061ea37b60fa5f988e41e237ae5fd46
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-04-21 15:50:27 -0400

    Update spark config

--- python/test/localPerformanceTests/zonalAveDemo.py
+++ python/test/localPerformanceTests/zonalAveDemo.py
@@ -14,14 +14,14 @@ try:
         portal.start_CDAS()
         time.sleep(20)
     else:
-        portal = CDASPortal(ConnectionMode.CONNECT, "localhost", request_port, response_port)
+        portal = CDASPortal(ConnectionMode.CONNECT, server, request_port, response_port)
 
     response_manager = portal.createResponseManager()
 
     t0 = time.time()
     datainputs = '[domain=[{"name":"d0"}],variable=[{"uri":"file:/home/tpmaxwel/.cdas/cache/cdscan/merra_mon_ua.xml","name":"ua:v1","domain":"d0"}],operation=[{"name":"python.numpyModule.ave","input":"v1","axes":"xt","filter":"DJF"}]]'
 #    datainputs = '[domain=[{"name":"d0"}],variable=[{"uri":"file:/att/gpfsfs/ffs2004/ppl/tpmaxwel/cdas/cache/collections/NCML/merra_mon_ua.xml","name":"ua:v1","domain":"d0"}],operation=[{"name":"python.numpyModule.wave","input":"v1","axes":"xt","filter":"DJF"}]]'
-    print "Sending request on port {0}: {1}".format( portal.request_port, datainputs ); sys.stdout.flush()
+    print "Sending request on port {0}, server {1}: {2}".format( portal.request_port, server, datainputs ); sys.stdout.flush()
     rId = portal.sendMessage( "execute", [ "CDSpark.workflow", datainputs, ""] )
     responses = response_manager.getResponses(rId)
     print "Completed OP in time {0}".format( time.time()-t0 ); sys.stdout.flush()
