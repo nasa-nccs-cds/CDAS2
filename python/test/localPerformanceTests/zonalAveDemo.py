bcab1577b25f2bddd07cd5d8bfca6e5229ba1686
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-05-08 17:31:42 -0400

    Update zad

--- python/test/localPerformanceTests/zonalAveDemo.py
+++ python/test/localPerformanceTests/zonalAveDemo.py
@@ -27,7 +27,7 @@ try:
     response_manager = portal.createResponseManager()
 
     t0 = time.time()
-    datainputs = '[domain=[{"name":"d0","lat":{"start":30,"end":40,"system":"indices"},"lon":{"start":30,"end":40,"system":"indices"},"time":{"start":0,"end":100,"system":"indices"}}],variable=[{"uri":"' + dataset + '","name":"ua:v1","domain":"d0"}],operation=[{"name":"CDSpark.average","input":"v1","axes":"xt","filter":"DJF"}]]'
+    datainputs = '[domain=[{"name":"d0","lat":{"start":30,"end":40,"system":"indices"},"lon":{"start":30,"end":40,"system":"indices"},"lev":{"start":20,"end":20,"system":"indices"},"time":{"start":0,"end":100,"system":"indices"}}],variable=[{"uri":"' + dataset + '","name":"ua:v1","domain":"d0"}],operation=[{"name":"CDSpark.max","input":"v1","axes":"xy"}]]'
 #   datainputs = '[domain=[{"name":"d0"}],variable=[{"uri":"' + dataset + '","name":"ua:v1","domain":"d0"}],operation=[{"name":"python.numpyModule.ave","input":"v1","axes":"xt","filter":"DJF"}]]'
     print "Sending request on port {0}, server {1}: {2}".format( portal.request_port, server, datainputs ); sys.stdout.flush()
     rId = portal.sendMessage( "execute", [ "CDSpark.workflow", datainputs, ""] )
