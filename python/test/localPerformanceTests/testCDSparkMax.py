e66802056cadd271347b7767d3414e80859e66be
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-05-08 17:48:34 -0400

    Add new test

---
+++ python/test/localPerformanceTests/testCDSparkMax.py
@@ -0,0 +1,35 @@
+from pycdas.portal.cdas import *
+import time, sys
+
+startServer = False
+portal = None
+request_port = 4356
+response_port = 4357
+
+try:
+
+    if startServer:
+        portal = CDASPortal()
+        portal.start_CDAS()
+        time.sleep(20)
+    else:
+        portal = CDASPortal(ConnectionMode.CONNECT, server, request_port, response_port)
+
+    response_manager = portal.createResponseManager()
+
+    t0 = time.time()
+    datainputs = '[domain=[{"name":"d0","time":{"start":50,"end":150,"system":"indices"},"lon":{"start":100,"end":100,"system":"indices"},"lat":{"start":10,"end":20,"system":"indices"} }],variable=[{"uri":"http://esgf.nccs.nasa.gov/thredds/dodsC/CMIP5/NASA/GISS/historical/E2-H_historical_r1i1p1/tas_Amon_GISS-E2-H_historical_r1i1p1_185001-190012.nc","name":"tas:v1","domain":"d0"}],operation=[{"name":"CDSpark.max","input":"v1","domain":"d0","axes":"t"}]]'
+    print "Sending request on port {0}, server {1}: {2}".format( portal.request_port, server, datainputs ); sys.stdout.flush()
+    rId = portal.sendMessage( "execute", [ "CDSpark.workflow", datainputs, ""] )
+    responses = response_manager.getResponses(rId)
+    print "Completed OP in time {0}".format( time.time()-t0 ); sys.stdout.flush()
+    print "Responses = " + str(responses)
+
+except Exception, err:
+    traceback.print_exc()
+
+finally:
+
+    portal.shutdown()
+
+
