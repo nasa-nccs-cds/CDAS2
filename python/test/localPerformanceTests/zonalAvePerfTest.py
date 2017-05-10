a39083ffa3b61592c0a03d1a1fd146fa0f7f4359
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-05-10 16:39:22 -0400

    Diagnostics

--- python/test/localPerformanceTests/zonalAvePerfTest.py
+++ python/test/localPerformanceTests/zonalAvePerfTest.py
@@ -20,7 +20,7 @@ else:
 
 cldraGISSdset = "file:///home/tpmaxwel/.cdas/cache/collections/NCML/GISS_E2H_r1i1p1.ncml"
 localGISSdset = "file:///Users/tpmaxwel/.cdas/cache/collections/NCML/giss_r1i1p1.xml"
-currentGISSDataset = localGISSdset
+currentGISSDataset = cldraGISSdset
 
 try:
 
