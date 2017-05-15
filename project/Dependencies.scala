ea66b09e46dd57844763c60381c9a71262caacbe
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-05-15 14:37:06 -0400

    Add time test

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,8 +33,8 @@ object Library {
   val concurrentlinkedhashmap = "com.googlecode.concurrentlinkedhashmap" % "concurrentlinkedhashmap-lru" % "1.4.2"
   val reflections    = "org.reflections" % "reflections"       % "0.9.10"
 
-  val scalaxml       = "org.scala-lang.modules" %% "scala-xml"  % "1.0.3"
-  val scalaparser    = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.3"
+  val scalaxml       = "org.scala-lang.modules" %% "scala-xml"  % "1.0.2"
+  val scalaparser    = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.2"
 }
 
 object Dependencies {
