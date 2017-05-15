df946a2491b9ad11c6a56521aa5ebdd07be2c23c
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-05-15 15:09:53 -0400

    Add time test

--- src/main/scala/nasa/nccs/cdas/portal/application.scala
+++ src/main/scala/nasa/nccs/cdas/portal/application.scala
@@ -104,12 +104,12 @@ object TestApplication extends Loggable {
     val parameter_file = elem(args, 3, "")
     val appConfiguration = getConfiguration( parameter_file )
     val cmode = if (connect_mode.toLowerCase.startsWith("c")) CONNECT else BIND
-    
+
     val sc = CDSparkContext()
     val indices = sc.sparkContext.parallelize( Array( 1, 2, 3, 4, 5 ,6 ,7 ) )
     val timings = indices.map( i => System.nanoTime() )
     val t0 = System.nanoTime()
-    val time_list = timings.collect().map( _.toString ) mkString (", ")
-    println( t0.toString + ": " + time_list )
+    val time_list = timings.collect().map( tval => (tval/1.0E9)_.toString ) mkString (", ")
+    println( (t0/1.0E9)_.toString + ": " + time_list )
   }
 }
