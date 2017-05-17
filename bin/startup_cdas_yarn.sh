8dd7702da5b7a50e9f4203448c92ee4723027d4e
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-05-17 12:27:33 -0400

    update test app

--- bin/startup_cdas_yarn.sh
+++ bin/startup_cdas_yarn.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-CDAS_JAR=${CDAS_HOME_DIR}/target/scala-2.11/cdas2_2.11-1.2.2-SNAPSHOT.jar
+CDAS_JAR=${CDAS_HOME_DIR}/target/scala-2.10/cdas2_2.10-1.2.2-SNAPSHOT.jar
 spark-submit --class nasa.nccs.cdas.portal.CDASApplication --master yarn --deploy-mode cluster --driver-memory 8g ${CDAS_JAR} bind 4356 4357
