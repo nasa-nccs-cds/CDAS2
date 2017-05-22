2542558bb1fb98e512dc5da751fc96d45601c359
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-05-22 13:06:47 -0400

    Add object & file modes

--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -190,12 +190,25 @@ class CurrentTestSuite extends FunSuite with Loggable with BeforeAndAfter {
       val nco_verified_result: CDFloatArray = CDFloatArray( Array( 140615.5f, 139952f, 139100.6f, 138552.2f, 137481.9f, 137100.5f ), Float.MaxValue )
       val datainputs = s"""[domain=[{"name":"d0","lat":{"start":5,"end":5,"system":"indices"},"lon":{"start":5,"end":10,"system":"indices"}}],variable=[{"uri":"http://esgf.nccs.nasa.gov/thredds/dodsC/CMIP5/NASA/GISS/historical/E2-H_historical_r1i1p1/tas_Amon_GISS-E2-H_historical_r1i1p1_185001-190012.nc","name":"tas:v1","domain":"d0","cache":"false"}],operation=[{"name":"CDSpark.sum","input":"v1","domain":"d0","axes":"t"}]]"""
       val result_node = executeTest(datainputs)
-      val result_data = getResultData( result_node )
-      println( "Op Result:       " + result_data )
-      println( "Verified Result: " + nco_verified_result )
-      assert( result_data.maxScaledDiff( nco_verified_result )  < eps, s" Incorrect value computed for Max")
+      print( result_node.toString() )
+
+//      val result_data = getResultData( result_node )
+//      println( "Op Result:       " + result_data )
+//      println( "Verified Result: " + nco_verified_result )
+//      assert( result_data.maxScaledDiff( nco_verified_result )  < eps, s" Incorrect value computed for Max")
     }
-    test("pyMaximum-cache") {
+
+  test("TimeSum-object") {
+    val nco_verified_result: CDFloatArray = CDFloatArray( Array( 140615.5f, 139952f, 139100.6f, 138552.2f, 137481.9f, 137100.5f ), Float.MaxValue )
+    val datainputs = s"""[domain=[{"name":"d0","lat":{"start":5,"end":5,"system":"indices"},"lon":{"start":5,"end":10,"system":"indices"}}],variable=[{"uri":"http://esgf.nccs.nasa.gov/thredds/dodsC/CMIP5/NASA/GISS/historical/E2-H_historical_r1i1p1/tas_Amon_GISS-E2-H_historical_r1i1p1_185001-190012.nc","name":"tas:v1","domain":"d0","cache":"false"}],operation=[{"name":"CDSpark.sum","input":"v1","domain":"d0","axes":"t"}]]"""
+    val result_node = executeTest( datainputs, Map( "response" -> "object" ) )
+    val result_data = getResultData( result_node )
+    println( "Op Result:       " + result_data )
+    println( "Verified Result: " + nco_verified_result )
+    assert( result_data.maxScaledDiff( nco_verified_result )  < eps, s" Incorrect value computed for Max")
+  }
+
+  test("pyMaximum-cache") {
       val nco_verified_result = 309.7112
       val datainputs = s"""[domain=[{"name":"d0","time":{"start":10,"end":10,"system":"indices"}}],variable=[{"uri":"collection:/giss_r1i1p1","name":"tas:v1","domain":"d0"}],operation=[{"name":"python.numpyModule.max","input":"v1","domain":"d0","axes":"xy"}]]"""
       val result_node = executeTest(datainputs)
