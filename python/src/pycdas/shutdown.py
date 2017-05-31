5e737bc332fefcf95dcfd17d9d56871ba4a5cf68
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-05-31 14:12:32 -0400

    Update python utils

--- python/src/pycdas/shutdown.py
+++ python/src/pycdas/shutdown.py
@@ -1,10 +1,3 @@
 import subprocess, signal, os
-p = subprocess.Popen(['ps', '-fu', '$USER', "|", "grep", "python"], stdout=subprocess.PIPE)
+p = subprocess.Popen(['pkill', '-u', '$USER', "python"], stdout=subprocess.PIPE)
 out, err = p.communicate()
-
-print "Killing zombie workers: "
-for line in out.splitlines():
-   if not ('shutdown' in line):
-     print " >> " + line
-     pid = int(line.split()[1])
-     os.kill(pid, signal.SIGKILL)
