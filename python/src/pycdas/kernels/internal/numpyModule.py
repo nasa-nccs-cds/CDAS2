Fix weighted average

python/src/pycdas/kernels/internal/numpyModule.py
@@ -1,6 +1,7 @@
 from pycdas.kernels.Kernel import Kernel, KernelSpec
 from pycdas.cdasArray import npArray
 import numpy as np
+import numpy.ma as ma
 import time
 
 class StdKernel(Kernel):
@@ -82,6 +83,7 @@ class WeightedAverageKernel(Kernel):
         wids = [ inputId + "_WEIGHTS_" for inputId in data_inputIds ]
         weight_inputIds = [ ( wid if (wid in available_inputIds) else None ) for wid in wids ]
         inputs_with_weights = zip( data_inputIds, weight_inputIds )
+        self.logger.info("@@@@ data_inputIds = " + str(data_inputIds) + ", weight_inputIds = " + str(weight_inputIds) + ", inputs = " + str(inputs) )
         results = []
         for input_pair in inputs_with_weights:
             input = inputs.get( input_pair[0] )  # npArray
@@ -89,12 +91,13 @@ class WeightedAverageKernel(Kernel):
             else :
                 weights = inputs.get( input_pair[1] ).array if( input_pair[1] != None ) else None
                 axes = self.getOrderedAxes(task,input)
-                self.logger.info("\n Executing average, input: " + str( input_pair[0] ) + ", task metadata = " + str(task.metadata) + " Input metadata: " + str( input.metadata ) )
+                self.logger.info("\n Executing average, input: " + str( input_pair[0] ) + ", shape = " + str(input.array.shape)+ ", task metadata = " + str(task.metadata) + " Input metadata: " + str( input.metadata ) )
                 t0 = time.time()
                 result = input.array
                 for axis in axes:
                     current_shape = list( result.shape )
-                    ( result, weights ) = np.ma.average( result, axis, weights, True )
+                    self.logger.info(" --> Exec: axis: " + str(axis) + ", shape: " + str(current_shape) )
+                    ( result, weights ) = ma.average( result, axis, np.broadcast_to( weights, current_shape ), True )
                     current_shape[axis] = 1
                     result = result.reshape( current_shape )
                     weights = weights.reshape( current_shape )