84f0944f3be09aa363aec79999bf784f6f4d6c15
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-06-17 12:59:06 -0400

    Update run script

--- bin/startup_cdas_standalone.sh
+++ bin/startup_cdas_standalone.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CDAS_JAR=${CDAS_HOME_DIR}/target/scala-2.10/cdas2_2.10-1.2.2-SNAPSHOT.jar 
+CDAS_JAR=${CDAS_HOME_DIR}/target/scala-2.11/cdas2_2.11-1.2.2-SNAPSHOT.jar
 SPARK_PRINT_LAUNCH_COMMAND=true 
 APP_DEP_JARS=$(find ~/.ivy2 -name "*.jar" )
 APP_DEP_CP=$(echo $APP_DEP_JARS | sed -r 's/[ \n\r]+/:/g')
