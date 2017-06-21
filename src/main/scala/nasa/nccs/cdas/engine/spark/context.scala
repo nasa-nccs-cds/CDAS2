d36c1a034b646a38bede7f0bc700aebec027dd82
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-06-21 14:52:06 -0400

    Update parts per node

--- src/main/scala/nasa/nccs/cdas/engine/spark/context.scala
+++ src/main/scala/nasa/nccs/cdas/engine/spark/context.scala
@@ -84,6 +84,7 @@ object CDSparkContext extends Loggable {
       .set("spark.local.dir", cdas_cache_dir )
       .set("spark.file.transferTo", "false" )
       .set("spark.kryoserializer.buffer.max", "1000m" )
+      .set("spark.network.timeout", "1800s" )
       .registerKryoClasses( Array(classOf[DirectRDDRecordSpec], classOf[RecordKey], classOf[RDDRecord], classOf[DirectRDDVariableSpec], classOf[CDSection], classOf[HeapFltArray], classOf[Partition], classOf[CDCoordMap] ) )
 
     if( enableMetrics ) sc.set("spark.metrics.conf", getClass.getResource("/spark.metrics.properties").getPath )
