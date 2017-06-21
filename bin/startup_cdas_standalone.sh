521387741ef73aef8fb4f0ec585ecf6e8b0042b2
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-06-21 17:24:27 -0400

    Update spark config

--- bin/startup_cdas_standalone.sh
+++ bin/startup_cdas_standalone.sh
@@ -6,5 +6,5 @@ APP_DEP_CP=$(echo $APP_DEP_JARS | sed -r 's/[ \n\r]+/:/g')
 echo "Application classpath:  $APP_DEP_CP "
 rm ~/.cdas/logs/cdas-* ~/.cdas/logs/python-*
 rm -rf $CDAS_CACHE_DIR/block* $CDAS_CACHE_DIR/spark*
-$SPARK_HOME/bin/spark-submit --class nasa.nccs.cdas.portal.CDASApplication --master spark://cldralogin101:7077 --deploy-mode client --conf "spark.executor.extraClassPath=${APP_DEP_CP}" --conf "spark.driver.extraClassPath=${APP_DEP_CP}" --driver-memory 3G ${CDAS_JAR} bind 4356 4357 ${CDAS_CACHE_DIR}/cdas.properties
+$SPARK_HOME/bin/spark-submit --class nasa.nccs.cdas.portal.CDASApplication --master spark://cldralogin101:7077 --deploy-mode client --conf "spark.executor.extraClassPath=${APP_DEP_CP}" --conf "spark.driver.extraClassPath=${APP_DEP_CP}" --driver-memory 10G ${CDAS_JAR} bind 4356 4357 ${CDAS_CACHE_DIR}/cdas.properties
 
