8e712d54775e2fac098b16ea411baf655a316ae7
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-06-24 13:30:01 -0400

    Add time sync test

--- bin/startup_spark_test.sh
+++ bin/startup_spark_test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 CDAS_JAR=${CDAS_HOME_DIR}/target/scala-2.10/cdas2_2.10-1.2.2-SNAPSHOT.jar
-spark-submit --class nasa.nccs.cdas.portal.TestApplication --master yarn --deploy-mode cluster --driver-memory 8g ${CDAS_JAR} bind 4356 4357
+APP_DEP_JARS=$(find ~/.ivy2 -name "*.jar" )
+APP_DEP_CP=$(echo $APP_DEP_JARS | sed -r 's/[ \n\r]+/:/g')
+$SPARK_HOME/bin/spark-submit --class nasa.nccs.cdas.portal.TestApplication --master spark://cldralogin101:7077 --deploy-mode client --conf "spark.executor.extraClassPath=${APP_DEP_CP}" --conf "spark.driver.extraClassPath=${APP_DEP_CP}" --driver-memory 10G ${CDAS_JAR} bind 4356 4357 ${CDAS_CACHE_DIR}/cdas.properties
