9a2f6ca45bcc2cfde9bcdd473ec5d5dcdd1cb2ca
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-07-12 16:54:14 -0400

    Add diurnal cycle test

---
+++ python/test/localPerformanceTests/DASSDiurnalCycle.py
@@ -0,0 +1,19 @@
+from pycdas.portal.cdas import *
+
+request_port = 5670
+response_port = 5671
+cdas_server = "10.71.9.11"
+
+try:
+    portal = CDASPortal( ConnectionMode.CONNECT, cdas_server, request_port, response_port )
+    response_manager = portal.createResponseManager()
+    datainputs = """[domain=[{"name":"d0","lat":{"start":70,"end":90,"system":"values"},"lon":{"start":5,"end":45,"system":"values"}}],variable=[{"uri":"file:///dass/nobackup/tpmaxwel/cdas/cache/collections/NCML/CIP_MERRA2_1hr_T2M.ncml","name":"T2M:v1","domain":"d0"}],operation=[{"name":"CDSpark.binAve","input":"v1","domain":"d0","cycle":"diurnal","bin":"month","axes":"t"}]]"""
+    rId1 = portal.sendMessage("execute", [ "WPS", datainputs, '{ "response":"object" }'] )
+    responses = response_manager.getResponseVariables(rId1)
+
+    print " Received %d responses ".format( len(responses) )
+
+finally:
+    portal.shutdown()
+
+
