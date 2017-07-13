528fe4fbf9211ef17f923ca0b1e9d9cd6179c745
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-07-13 14:35:13 -0400

    Diagnostics

--- python/test/localPerformanceTests/LocalDiurnalCycle.py
+++ python/test/localPerformanceTests/LocalDiurnalCycle.py
@@ -2,12 +2,12 @@ from pycdas.portal.cdas import *
 
 request_port = 5670
 response_port = 5671
-cdas_server = "10.71.9.11"
+cdas_server = "localhost"
 
 try:
     portal = CDASPortal( ConnectionMode.CONNECT, cdas_server, request_port, response_port )
     response_manager = portal.createResponseManager()
-    datainputs = """[domain=[{"name":"d0","time":{"start":0,"end":3000,"system":"indices"},"lat":{"start":70,"end":90,"system":"values"},"lon":{"start":25,"end":45,"system":"values"}}],variable=[{"uri":"file:///dass/nobackup/tpmaxwel/cdas/cache/collections/NCML/MERRA-TAS1hr.ncml","name":"tas:v1","domain":"d0"}],operation=[{"name":"CDSpark.binAve","input":"v1","domain":"d0","cycle":"diurnal","bin":"month","axes":"t"}]]"""
+    datainputs = """[domain=[{"name":"d0","time":{"start":0,"end":3000,"system":"indices"},"lat":{"start":70,"end":90,"system":"values"},"lon":{"start":25,"end":45,"system":"values"}}],variable=[{"uri":"file:////Users/tpmaxwel/.cdas/cache/collections/NCML/MERRA2-6hr-ana_Np.200001.ncml","name":"T:v1","domain":"d0"}],operation=[{"name":"CDSpark.binAve","input":"v1","domain":"d0","cycle":"diurnal","bin":"month","axes":"t"}]]"""
     rId1 = portal.sendMessage("execute", [ "WPS", datainputs, '{ "response":"object" }'] )
     responses = response_manager.getResponseVariables(rId1)
 
