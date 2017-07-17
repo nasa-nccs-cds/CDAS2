939447b7fcd6dffe136ed1ab25e1194d934b0440
tpmaxwel@nccs.nasa.gov <tpmaxwel@nccs.nasa.gov>
2017-07-17 17:28:02 -0400

    Add python ave test

--- python/test/localPerformanceTests/DASSPlotTestDebug.py
+++ python/test/localPerformanceTests/DASSPlotTestDebug.py
@@ -12,7 +12,7 @@ cdas_server = "10.71.9.11"
 try:
     portal = CDASPortal( ConnectionMode.CONNECT, cdas_server, request_port, response_port )
     response_manager = portal.createResponseManager()
-    datainputs = """[domain=[{"name":"d0","lat":{"start":70,"end":90,"system":"values"},"lon":{"start":5,"end":45,"system":"values"}}],variable=[{"uri":"file:///dass/nobackup/tpmaxwel/cdas/cache/collections/NCML/CIP_MERRA2_6hr_tas.ncml","name":"tas:v1","domain":"d0"}],operation=[{"name":"CDSpark.average","input":"v1","domain":"d0","axes":"xy"}]]"""
+    datainputs = """[domain=[{"name":"d0","lat":{"start":70,"end":90,"system":"values"},"lon":{"start":5,"end":45,"system":"values"}}],variable=[{"uri":"file:///dass/nobackup/tpmaxwel/cdas/cache/collections/NCML/CIP_MERRA2_6hr_tas.ncml","name":"tas:v1","domain":"d0"}],operation=[{"name":"python.numpyModule.ave","input":"v1","domain":"d0","axes":"xy"}]]"""
 
     rId1 = portal.sendMessage("execute", [ "WPS", datainputs, '{ "response":"file" }'] )
     fileResponses = response_manager.getResponseVariables(rId1)
